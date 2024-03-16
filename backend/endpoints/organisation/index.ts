a6547b5f4994e1e33e1d33947f9d0fc8ebbfa951
Vasek - Tom C <tom.chauveau@epitech.eu>
2024-03-16 18:19:41 +0000

    refactor: improve organisation joining logic
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- backend/endpoints/organisation/index.ts
+++ backend/endpoints/organisation/index.ts
@@ -7,45 +7,47 @@ import httpStatus from "http-status";
 const router = Router();
 
 router.post("/join", bodyParser.json(), async (req, res, next) => {
-    if (!req.body.name) {
-        return next(new Error("field name is missing from request body."));
-    }
-    
-    if (!req.body.userID) {
-        return next(new Error("field userID is missing from request body."));
-    }
-    
-    try {
-        const org = await ctx.prisma.organisation.findFirst({ where: { name: req.body.name } });
-        if (!org?.id) {
-        return next(new Error(`organisation ${req.body.name} not found.`));
-        }
-    
-        await ctx.prisma.organisation.update({
-        where: { id: org.id },
-        data: { users: { connect: [{ id: req.body.userID }] } },
-        });
-    
-        return res.status(httpStatus.OK).json({ message: "User joined organisation!", org });
-    } catch (error) {
-        return next(new Error("could not join organisation.", { cause: error }));
-    }
-})
-
-router.post("", bodyParser.json(), async (req, res, next) => {
-  if (!req.body.name) {
+  const { name, userID } = req.body;
+  if (!name) {
     return next(new Error("field name is missing from request body."));
   }
 
-  if (!req.body.userID) {
+  if (!userID) {
+    return next(new Error("field userID is missing from request body."));
+  }
+
+  try {
+    const org = await ctx.prisma.organisation.findFirst({ where: { name: name } });
+    if (!org?.id) {
+      return next(new Error(`organisation ${name} not found.`));
+    }
+
+    await ctx.prisma.organisation.update({
+      where: { id: org.id },
+      data: { users: { connect: [{ id: userID }] } },
+    });
+
+    return res.status(httpStatus.OK).json({ message: "User joined organisation!", org });
+  } catch (error) {
+    return next(new Error("could not join organisation.", { cause: error }));
+  }
+});
+
+router.post("", bodyParser.json(), async (req, res, next) => {
+  const { name, userID } = req.body;
+  if (!name) {
+    return next(new Error("field name is missing from request body."));
+  }
+
+  if (!userID) {
     return next(new Error("field userID is missing from request body."));
   }
 
   try {
     const org = await ctx.prisma.organisation.create({
-      data: { name: req.body.name, users: { connect: [{ id: req.body.userID }] } },
+      data: { name: name, users: { connect: [{ id: userID }] } },
     });
-    
+
     return res.status(httpStatus.CREATED).json({ message: "Organisation created!", org });
   } catch (error) {
     return next(new Error("could not create organisation.", { cause: error }));
--- backend/endpoints/user/index.ts
+++ backend/endpoints/user/index.ts
@@ -1,76 +1,97 @@
-import { Router } from 'express'
+import { Router } from "express";
 import bodyParser from "body-parser";
 
-import ctx from '@context'
-import httpStatus from 'http-status';
-import type { User as CircleUser, Session } from '@service/circle/types';
+import ctx from "@context";
+import httpStatus from "http-status";
+import type { User as CircleUser, Session } from "@service/circle/types";
 
-const router = Router()
+const router = Router();
 
-router.post('/connect', bodyParser.json(), async (req, res, next) => {
-    if (!req.body.name) {
-        return next(new Error("field name is missing from request body."))
+router.post("/connect", bodyParser.json(), async (req, res, next) => {
+  const name = req.body.name;  
+  if (!name) {
+    return next(new Error("field name is missing from request body."));
+  }
+
+  let userID: string;
+  try {
+    const res = await ctx.prisma.user.findFirst({ where: { name: name } });
+    if (!res?.id) {
+      return next(new Error(`user ${name} not found.`));
     }
 
-    let userID: string
-    try {
-        const res = await ctx.prisma.user.findFirst({ where: { name: req.body.name } })
-        if (!res?.id) {
-            return next(new Error(`user ${req.body.name} not found.`))
-        }
+    userID = res.id;
+  } catch (error) {
+    return next(new Error("could not find user in database.", { cause: error }));
+  }
 
-        userID = res.id
-    } catch (error) {
-        return next(new Error("could not find user in database.", { cause: error }))
-    }
+  // Connect to an existing user on Circle API
+  try {
+    const session = await ctx.circleSDK.connectUser({ userID });
 
-    // Connect to an existing user on Circle API
-    try {
-        const session = await ctx.circleSDK.connectUser({ userID })
+    return res.status(httpStatus.OK).json({ message: "User connected!", session });
+  } catch (error) {
+    return next(new Error("could not connect to user.", { cause: error }));
+  }
+});
 
-        return res.status(httpStatus.OK).json({ message: "User connected!", session })
-    } catch (error) {
-        return next(new Error("could not connect to user.", { cause: error }))
-    }
-})
+router.post("/register", bodyParser.json(), async (req, res, next) => {
+  const name = req.body.name;
+  if (!name) {
+    return next(new Error("field name is missing from request body."));
+  }
 
-router.post('/register', bodyParser.json(), async (req, res, next) => {
-    if (!req.body.name) {
-        return next(new Error("field name is missing from request body."))
-    }
+  // Create a new user on Circle API
+  let circleUser: CircleUser;
+  try {
+    circleUser = await ctx.circleSDK.createUser({ name: name });
+  } catch (error) {
+    return next(new Error("could not create user.", { cause: error }));
+  }
 
-    // Create a new user on Circle API
-    let circleUser: CircleUser
-    try {
-        circleUser = await ctx.circleSDK.createUser({ name: req.body.name })
-    } catch (error) {
-        return next(new Error("could not create user.", { cause: error }))
-    }
+  try {
+    // Save the user to the database
+    const user = await ctx.prisma.user.create({
+      data: { id: circleUser.userID, name: req.body.name },
+    });
 
-    try {
-        // Save the user to the database
-        const user = await ctx.prisma.user.create({
-            data: { id: circleUser.userID, name: req.body.name },
-        })
+    return res.status(httpStatus.CREATED).json({ message: "User created!", circleUser });
+  } catch (error) {
+    return next(new Error("could not save user to database.", { cause: error }));
+  }
+});
 
-        return res.status(httpStatus.CREATED).json({ message: "User created!", circleUser })
-    } catch (error) {
-        return next(new Error("could not save user to database.", { cause: error }))
-    }
-})
+router.get("/:userID/organisation", async (req, res, next) => {
+  const userID = req.params.userID;
+  if (!userID) {
+    return next(new Error("field userID is missing from request body."));
+  }
 
-router.get("", async (req, res, next) => {
-    if (!req.body.userID) {
-        return next(new Error("field userID is missing from request body."))
-    }
+  try {
+    const userOrganisation = await ctx.prisma.user.findFirst({
+      where: { id: userID },
+      select: { organisations: true },
+    });
 
-    try {
-        const user = await ctx.circleSDK.getUser(req.body.userID)
+    return res.status(httpStatus.OK).json({ userOrganisation });
+  } catch (error) {
+    return next(new Error("could not get user organisation.", { cause: error }));
+  }
+});
 
-        return res.status(httpStatus.OK).json({ user })
-    } catch (error) {
-        return next(new Error("could not get user.", { cause: error }))
-    }
-})
+router.get("/:userID", async (req, res, next) => {
+  const userID = req.params.userID;
+  if (!userID) {
+    return next(new Error("field userID is missing from request body."));
+  }
 
-export default router
+  try {
+    const user = await ctx.circleSDK.getUser(userID);
+
+    return res.status(httpStatus.OK).json({ user });
+  } catch (error) {
+    return next(new Error("could not get user.", { cause: error }));
+  }
+});
+
+export default router;
