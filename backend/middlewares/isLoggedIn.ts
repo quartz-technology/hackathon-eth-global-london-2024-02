f0c5e0ed6fa8e66a39f975afa5b1406934840402
Vasek - Tom C <tom.chauveau@epitech.eu>
2024-03-17 00:50:53 +0000

    chore: add loggin for authentication
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- backend/middlewares/isLoggedIn.ts
+++ backend/middlewares/isLoggedIn.ts
@@ -5,7 +5,7 @@ export function isLoggedIn(req: any, res: Response, next: any) {
         console.debug("User is logged in.")
         next();
     } else {
-        console.debug("User is not logged in.")
+        console.debug("User is not logged in.", req.session.userID, req.session.userToken, req.session.walletID, req.session.walletAddress)
         return res.status(401).json({ message: "Unauthorized" });
     }
 }
